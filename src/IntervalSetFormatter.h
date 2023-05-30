5f611d45353ce9441ec78995247f4d86eec7fc9e
Chamber6821 <vania.osinin@yandex.ru>
2023-05-30 17:24:01 +0300

    feat: add IntervalSetFormatter

---
+++ src/IntervalSetFormatter.h
@@ -0,0 +1,17 @@
+//
+// Created by Ivan on 30.05.2023.
+//
+
+#pragma once
+
+#include <format>
+#include <pubgrub/interval.hpp>
+
+template <>
+struct std::formatter<pubgrub::interval_set<int>> : std::formatter<std::string> {
+    auto format(auto interval, format_context& ctx) {
+        std::stringstream str;
+        str << interval;
+        return formatter<string>::format(str.str(), ctx);
+    }
+};
